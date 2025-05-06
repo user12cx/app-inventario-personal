f64b80511348ec9a642d269ce71cf73c28bd9f55
Ivan <ichimbobautista@gmail.com>
2025-05-06 18:29:39 -0500

    cambios  de sql server precedure almacener

--- SQL/producerAlmacener.sql
+++ SQL/producerAlmacener.sql
@@ -7,15 +7,19 @@ BEGIN
     SET NOCOUNT ON;
 
     -- Verificar si el email ya está registrado
-    IF EXISTS (SELECT 1 FROM Usuarios WHERE email = @email)
+    IF EXISTS (SELECT 1
+    FROM Usuarios
+    WHERE email = @email)
     BEGIN
         RAISERROR('El email ya está registrado', 16, 1);
         RETURN;
     END
 
     -- Insertar el nuevo usuario con la contraseña cifrada
-    INSERT INTO Usuarios (usuario, email, password)
-    VALUES (@usuario, @email, @password);
+    INSERT INTO Usuarios
+        (usuario, email, password)
+    VALUES
+        (@usuario, @email, @password);
 END;
 go
 
@@ -36,25 +40,25 @@ go
 -------------------------------------
 
 CREATE PROCEDURE [dbo].[getTransaccionesPaginadas]
-    @usuario_id INT  
+    @usuario_id INT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT 
-        t.idTransaccion, 
-        t.usuario_id, 
-        t.descripcion, 
-        c.nombre AS categoria, 
-        t.monto, 
-        t.tipo, 
-        cu.nombre AS cuenta, 
+    SELECT
+        t.idTransaccion,
+        t.usuario_id,
+        t.descripcion,
+        c.nombre AS categoria,
+        t.monto,
+        t.tipo,
+        cu.nombre AS cuenta,
         t.fecha
     FROM Transacciones t
-    INNER JOIN Categorias c ON t.categoria_id = c.idCategoria 
-    INNER JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta 
-    WHERE t.usuario_id = @usuario_id  
-    ORDER BY t.idTransaccion DESC;  
+        INNER JOIN Categorias c ON t.categoria_id = c.idCategoria
+        INNER JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta
+    WHERE t.usuario_id = @usuario_id
+    ORDER BY t.idTransaccion DESC;
 END;
 go
 
@@ -66,18 +70,18 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT TOP 3 
-        t.idTransaccion, 
-        t.usuario_id, 
-        t.descripcion, 
-        c.nombre AS categoria,  
-        t.monto, 
-        t.tipo, 
-        cu.nombre AS cuenta,  
+    SELECT TOP 3
+        t.idTransaccion,
+        t.usuario_id,
+        t.descripcion,
+        c.nombre AS categoria,
+        t.monto,
+        t.tipo,
+        cu.nombre AS cuenta,
         t.fecha
     FROM Transacciones t
-    JOIN Categorias c ON t.categoria_id = c.idCategoria
-    JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta
+        JOIN Categorias c ON t.categoria_id = c.idCategoria
+        JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta
     WHERE t.usuario_id = @usuario_id
     ORDER BY t.fecha DESC;
 END;
@@ -85,9 +89,12 @@ go
 -----------------------------------------
 
 CREATE PROCEDURE [dbo].[getObjetivosFuturos]
+    @usuario_id int 
 AS
 BEGIN
-    SELECT * FROM ObjetivosAhorro;
+    SELECT *
+    FROM ObjetivosAhorro
+    where usuario_id= @usuario_id
 END;
 go
 
@@ -99,13 +106,14 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT 
-        LEFT(DATENAME(MONTH, t.fecha), 3) + ' ' + CAST(YEAR(t.fecha) AS VARCHAR) AS mes, 
+    SELECT
+        LEFT(DATENAME(MONTH, t.fecha), 3) + ' ' + CAST(YEAR(t.fecha) AS VARCHAR) AS mes,
         SUM(t.monto) AS total_gasto
     FROM Transacciones t
-    INNER JOIN TipoTransaccion tt ON t.tipo_id = tt.id_tipo
+        INNER JOIN TipoTransaccion tt ON t.tipo_id = tt.id_tipo
     WHERE t.usuario_id = @usuario_id
-      AND tt.nombre = 'gasto'  -- ← ahora se filtra por nombre del tipo
+        AND tt.nombre = 'gasto'
+    -- ← ahora se filtra por nombre del tipo
     GROUP BY YEAR(t.fecha), MONTH(t.fecha), DATENAME(MONTH, t.fecha)
     ORDER BY YEAR(t.fecha), MONTH(t.fecha);
 END;
@@ -118,8 +126,9 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT * 
-    FROM Cuentas WHERE usuario_id = @usuario_id;
+    SELECT *
+    FROM Cuentas
+    WHERE usuario_id = @usuario_id;
 END;
 go
 --------------------------------------------
@@ -130,7 +139,7 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT * 
+    SELECT *
     FROM Categorias
     WHERE usuario_id = @usuario_id;
 END;
@@ -138,9 +147,9 @@ go
 -------------------------------------------
 
 CREATE PROCEDURE [dbo].[gestionarCategorias]
-    @accion INT, 
+    @accion INT,
     @usuario_id INT,
-    @categoria_id INT = NULL, 
+    @categoria_id INT = NULL,
     @nombre NVARCHAR(100) = NULL,
     @tipo NVARCHAR(50) = NULL
 AS
@@ -155,8 +164,10 @@ BEGIN
 
     IF @accion = 1
     BEGIN
-        INSERT INTO Categorias (usuario_id, nombre, tipo) 
-        VALUES (@usuario_id, @nombre, @tipo);
+        INSERT INTO Categorias
+            (usuario_id, nombre, tipo)
+        VALUES
+            (@usuario_id, @nombre, @tipo);
     END
     ELSE IF @accion = 2
     BEGIN
@@ -184,7 +195,9 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    IF EXISTS (SELECT 1 FROM categorias WHERE idCategoria = @id)
+    IF EXISTS (SELECT 1
+    FROM categorias
+    WHERE idCategoria = @id)
     BEGIN
         DELETE FROM Categorias WHERE idCategoria = @id;
         PRINT 'La categoría ha sido eliminada correctamente.';
@@ -206,54 +219,59 @@ AS
 BEGIN
     SET NOCOUNT ON;
 
-    INSERT INTO Categorias (usuario_id, nombre, tipo)
-    VALUES (@usuario_id, @nombre, @tipo);
+    INSERT INTO Categorias
+        (usuario_id, nombre, tipo)
+    VALUES
+        (@usuario_id, @nombre, @tipo);
 END;
 go
 
 ALTER PROCEDURE [dbo].[getTransaccionesPaginadas]
-    @usuario_id INT  
+    @usuario_id INT
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT 
-        t.idTransaccion, 
-        t.usuario_id, 
-        t.descripcion, 
-        c.nombre AS categoria, 
-        t.monto, 
-        t.tipo, 
-        cu.nombre AS cuenta, 
+    SELECT
+        t.idTransaccion,
+        t.usuario_id,
+        t.descripcion,
+        c.nombre AS categoria,
+        t.monto,
+        t.tipo,
+        cu.nombre AS cuenta,
         t.fecha
     FROM Transacciones t
-    INNER JOIN Categorias c ON t.categoria_id = c.idCategoria 
-    INNER JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta 
-    WHERE t.usuario_id = @usuario_id  
-    ORDER BY t.idTransaccion DESC;  
+        INNER JOIN Categorias c ON t.categoria_id = c.idCategoria
+        INNER JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta
+    WHERE t.usuario_id = @usuario_id
+    ORDER BY t.idTransaccion DESC;
 END
-GO 
+GO
 
 CREATE PROCEDURE [dbo].[getTransacciones]
-    @usuario_id INT  -- Se elimina el valor por defecto NULL
+    @usuario_id INT
+-- Se elimina el valor por defecto NULL
 AS
 BEGIN
     SET NOCOUNT ON;
 
-    SELECT TOP 3 
-        t.idTransaccion, 
-        t.usuario_id, 
-        t.descripcion, 
-        c.nombre AS categoria,  
-        t.monto, 
-        t.tipo, 
-        cu.nombre AS cuenta,  
+    SELECT TOP 3
+        t.idTransaccion,
+        t.usuario_id,
+        t.descripcion,
+        c.nombre AS categoria,
+        t.monto,
+        t.tipo,
+        cu.nombre AS cuenta,
         t.fecha
     FROM Transacciones t
-    JOIN Categorias c ON t.categoria_id = c.idCategoria
-    JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta
-    WHERE t.usuario_id = @usuario_id  -- Se mantiene la condición del usuario
-    ORDER BY t.fecha DESC;  -- Se ordena por fecha de más reciente a más antigua
+        JOIN Categorias c ON t.categoria_id = c.idCategoria
+        JOIN Cuentas cu ON t.cuenta_id = cu.idCuenta
+    WHERE t.usuario_id = @usuario_id
+    -- Se mantiene la condición del usuario
+    ORDER BY t.fecha DESC;
+-- Se ordena por fecha de más reciente a más antigua
 END;
 GO
 
@@ -270,12 +288,16 @@ BEGIN
     SET NOCOUNT ON;
 
     -- Insertar la transacción
-    INSERT INTO Transacciones (descripcion, categoria_id, monto, tipo_id, cuenta_id, fecha, usuario_id)
-    VALUES (@descripcion, @categoria_id, @monto, @tipo_id, @cuenta_id, @fecha, @usuario_id);
+    INSERT INTO Transacciones
+        (descripcion, categoria_id, monto, tipo_id, cuenta_id, fecha, usuario_id)
+    VALUES
+        (@descripcion, @categoria_id, @monto, @tipo_id, @cuenta_id, @fecha, @usuario_id);
 
     -- Obtener si el tipo es 'gasto' o 'ingreso'
     DECLARE @nombreTipo VARCHAR(50);
-    SELECT @nombreTipo = nombre FROM TipoTransaccion WHERE id_tipo = @tipo_id;
+    SELECT @nombreTipo = nombre
+    FROM TipoTransaccion
+    WHERE id_tipo = @tipo_id;
 
     -- Actualizar el saldo de la cuenta según el tipo
     IF @nombreTipo = 'gasto'
@@ -295,56 +317,63 @@ go
 
 -- aqui actualiza edita y elimina  metas a futuro
 
-CREATE PROCEDURE sp_objetivos_ahorro_tipo
-    @tipo            VARCHAR(10),      -- 'agregar', 'editar', 'eliminar'
-    @idObjetivo      INT = NULL,       -- solo necesario para editar o eliminar
-    @nombre          VARCHAR(100) = NULL,
-    @monto_objetivo  DECIMAL(10, 2) = NULL,
-    @monto_actual    DECIMAL(10, 2) = NULL,
-    @fecha_limite    DATE = NULL,
-    @usuario_id      INT = NULL
-AS
-BEGIN
-    SET NOCOUNT ON;
+-- CREATE PROCEDURE sp_objetivos_ahorro_tipo
+--     @tipo            VARCHAR(10),
+--     -- 'agregar', 'editar', 'eliminar'
+--     @idObjetivo      INT = NULL,
+--     -- solo necesario para editar o eliminar
+--     @nombre          VARCHAR(100) = NULL,
+--     @monto_objetivo  DECIMAL(10, 2) = NULL,
+--     @monto_actual    DECIMAL(10, 2) = NULL,
+--     @fecha_limite    DATE = NULL,
+--     @usuario_id      INT = NULL
+-- AS
+-- BEGIN
+--     SET NOCOUNT ON;
 
-    IF @tipo = 'agregar'
-    BEGIN
-        INSERT INTO ObjetivosAhorro (nombre, monto_objetivo, monto_actual, fecha_limite, usuario_id)
-        VALUES (@nombre, ISNULL(@monto_objetivo, 0), ISNULL(@monto_actual, 0), @fecha_limite, @usuario_id);
+--     IF @tipo = 'agregar'
+--     BEGIN
+--         INSERT INTO ObjetivosAhorro
+--             (nombre, monto_objetivo, monto_actual, fecha_limite, usuario_id)
+--         VALUES
+--             (@nombre, ISNULL(@monto_objetivo, 0), ISNULL(@monto_actual, 0), @fecha_limite, @usuario_id);
 
-        SELECT SCOPE_IDENTITY() AS idNuevoObjetivo; -- devuelve el id insertado
-    END
-    ELSE IF @tipo = 'editar'
-    BEGIN
-        UPDATE ObjetivosAhorro
-        SET nombre = @nombre,
-            monto_objetivo = @monto_objetivo,
-            monto_actual = @monto_actual,
-            fecha_limite = @fecha_limite,
-            usuario_id = @usuario_id
-        WHERE idObjetivo = @idObjetivo;
+--         SELECT SCOPE_IDENTITY() AS idNuevoObjetivo;
+--     -- devuelve el id insertado
+--     END
+--     ELSE IF @tipo = 'editar'
+--     BEGIN
+--         UPDATE ObjetivosAhorro
+--         SET nombre = @nombre,
+--             monto_objetivo = @monto_objetivo,
+--             monto_actual = @monto_actual,
+--             fecha_limite = @fecha_limite,
+--             usuario_id = @usuario_id
+--         WHERE idObjetivo = @idObjetivo;
 
-        SELECT 'Objetivo actualizado correctamente' AS mensaje;
-    END
-    ELSE IF @tipo = 'eliminar'
-    BEGIN
-        DELETE FROM ObjetivosAhorro
-        WHERE idObjetivo = @idObjetivo;
+--         SELECT 'Objetivo actualizado correctamente' AS mensaje;
+--     END
+--     ELSE IF @tipo = 'eliminar'
+--     BEGIN
+--         DELETE FROM ObjetivosAhorro
+--         WHERE idObjetivo = @idObjetivo;
 
-        SELECT 'Objetivo eliminado correctamente' AS mensaje;
-    END
-    ELSE
-    BEGIN
-        SELECT 'Tipo de operación no válido. Usa: agregar, editar o eliminar.' AS error;
-    END
-END
-go
+--         SELECT 'Objetivo eliminado correctamente' AS mensaje;
+--     END
+--     ELSE
+--     BEGIN
+--         SELECT 'Tipo de operación no válido. Usa: agregar, editar o eliminar.' AS error;
+--     END
+-- END
+-- go
 
 -- este es para las cuentas 
 
 CREATE PROCEDURE sp_cuentas_tipo
-    @tipo         VARCHAR(10),       -- 'agregar', 'editar', 'eliminar'
-    @idCuenta     INT = NULL,        -- necesario para editar o eliminar
+    @tipo         VARCHAR(10),
+    -- 'agregar', 'editar', 'eliminar'
+    @idCuenta     INT = NULL,
+    -- necesario para editar o eliminar
     @nombre       VARCHAR(100) = NULL,
     @saldo        MONEY = NULL,
     @estado       VARCHAR(10) = NULL,
@@ -355,10 +384,13 @@ BEGIN
 
     IF @tipo = 'agregar'
     BEGIN
-        INSERT INTO Cuentas (nombre, saldo, estado, usuario_id)
-        VALUES (@nombre, ISNULL(@saldo, 0), @estado, @usuario_id);
+        INSERT INTO Cuentas
+            (nombre, saldo, estado, usuario_id)
+        VALUES
+            (@nombre, ISNULL(@saldo, 0), @estado, @usuario_id);
 
-        SELECT SCOPE_IDENTITY() AS idNuevaCuenta; -- devuelve ID insertado
+        SELECT SCOPE_IDENTITY() AS idNuevaCuenta;
+    -- devuelve ID insertado
     END
     ELSE IF @tipo = 'editar'
     BEGIN
@@ -390,8 +422,10 @@ go
 
 
 CREATE PROCEDURE sp_usuarios_tipo
-    @tipo            VARCHAR(10),       -- 'agregar', 'editar', 'eliminar', 'listar'
-    @idUser          INT = NULL,        -- para editar, eliminar o listar por ID
+    @tipo            VARCHAR(10),
+    -- 'agregar', 'editar', 'eliminar', 'listar'
+    @idUser          INT = NULL,
+    -- para editar, eliminar o listar por ID
     @usuario         VARCHAR(20) = NULL,
     @name            VARCHAR(70) = NULL,
     @apellidos       VARCHAR(100) = NULL,
@@ -405,8 +439,10 @@ BEGIN
 
     IF @tipo = 'agregar'
     BEGIN
-        INSERT INTO Usuarios (usuario, name, apellidos, email, password, telefono, ocupacion)
-        VALUES (@usuario, @name, @apellidos, @email, @password, @telefono, @ocupacion);
+        INSERT INTO Usuarios
+            (usuario, name, apellidos, email, password, telefono, ocupacion)
+        VALUES
+            (@usuario, @name, @apellidos, @email, @password, @telefono, @ocupacion);
 
         SELECT SCOPE_IDENTITY() AS idNuevoUsuario;
     END
@@ -434,9 +470,12 @@ BEGIN
     ELSE IF @tipo = 'listar'
     BEGIN
         IF @idUser IS NULL
-            SELECT * FROM Usuarios;
+            SELECT *
+        FROM Usuarios;
         ELSE
-            SELECT * FROM Usuarios WHERE idUser = @idUser;
+            SELECT *
+        FROM Usuarios
+        WHERE idUser = @idUser;
     END
     ELSE
     BEGIN
@@ -512,63 +551,5 @@ BEGIN
         -- Eliminar objetivo
         DELETE FROM ObjetivosAhorro WHERE idObjetivo = @idObjetivo;
     END
-    ELSE IF @tipo = 'listar'
-    BEGIN
-        SELECT * FROM ObjetivosAhorro WHERE usuario_id = @usuario_id;
-    END
+  
 END
-
-BEGIN
-    SET NOCOUNT ON;
-
-    IF @tipo = 'agregar'
-    BEGIN
-        -- Restar monto de la cuenta
-        UPDATE Cuentas
-        SET saldo = saldo - ISNULL(@monto_actual, 0)
-        WHERE idCuenta = @cuenta_id;
-
-        -- Crear objetivo
-        INSERT INTO ObjetivosAhorro (nombre, monto_objetivo, monto_actual, fecha_limite, usuario_id, cuenta_id)
-        VALUES (@nombre, @monto_objetivo, @monto_actual, @fecha_limite, @usuario_id, @cuenta_id);
-    END
-    ELSE IF @tipo = 'editar'
-    BEGIN
-        DECLARE @monto_anterior DECIMAL(10,2);
-        SELECT @monto_anterior = monto_actual FROM ObjetivosAhorro WHERE idObjetivo = @idObjetivo;
-
-        -- Calcular diferencia
-        DECLARE @diferencia DECIMAL(10,2) = ISNULL(@monto_actual, 0) - ISNULL(@monto_anterior, 0);
-
-        -- Restar o sumar a la cuenta
-        UPDATE Cuentas
-        SET saldo = saldo - @diferencia
-        WHERE idCuenta = @cuenta_id;
-
-        -- Actualizar objetivo
-        UPDATE ObjetivosAhorro
-        SET nombre = @nombre,
-            monto_objetivo = @monto_objetivo,
-            monto_actual = @monto_actual,
-            fecha_limite = @fecha_limite
-        WHERE idObjetivo = @idObjetivo;
-    END
-    ELSE IF @tipo = 'eliminar'
-    BEGIN
-        DECLARE @monto_actual_eliminar DECIMAL(10,2);
-        SELECT @monto_actual_eliminar = monto_actual FROM ObjetivosAhorro WHERE idObjetivo = @idObjetivo;
-
-        -- Devolver fondos
-        UPDATE Cuentas
-        SET saldo = saldo + @monto_actual_eliminar
-        WHERE idCuenta = (SELECT cuenta_id FROM ObjetivosAhorro WHERE idObjetivo = @idObjetivo);
-
-        -- Eliminar objetivo
-        DELETE FROM ObjetivosAhorro WHERE idObjetivo = @idObjetivo;
-    END
-    ELSE IF @tipo = 'listar'
-    BEGIN
-        SELECT * FROM ObjetivosAhorro WHERE usuario_id = @usuario_id;
-    END
-END
-go
